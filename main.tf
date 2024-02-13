619134fc0e3b29b497bdad5d31198aff695db94d
cl-kazumi-nakajima <kazumeat@gmail.com>
2024-02-13 22:20:25 +0900

    2章: ロードバランサのデプロイ

--- main.tf
+++ main.tf
@@ -66,6 +66,10 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnets.default.ids
 
+  # ASGとALBのインテグレーション
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB" # ALBのヘルスチェックを行い、正常でない場合に自動的に置き換える指示をASGに出す
+
   min_size = 2
   max_size = 10
 
@@ -87,4 +91,88 @@ data "aws_subnets" "default" {
     name = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  # subnets = data.aws_subnet_ids.default.ids # 誤植？
+  subnets = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  # デフォルトではシンプルな404エラーページを返す
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page Not Found"
+      status_code = "404"
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # インバウンドHTTPリクエストを許可
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # アウトバウンドリクエストを全て許可
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# リクエストを、ASGが含まれるターゲットグループに送る、リスナールール
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
